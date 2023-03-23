041b9cef1c7089b10d70e27a560ef93780742825
YasAit <yassirx2012@gmail.com>
2023-03-23 14:30:36 +0100

    FormationGET/Apigateway

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -2,7 +2,10 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { join } from 'path';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -71,13 +74,45 @@ dbSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3306), 'Allow in
       value: instance.secret?.secretName!,
     });
 
+    //create a lambda function to connect to the database and run Select query
+    const FormationGet = new NodejsFunction(this, 'FormationGet', {
+      entry: join(__dirname, '../lambdas/Get.ts'),
+      handler: 'handler',
+      bundling: {
+        externalModules: ['aws-sdk'],
+      },
+      environment: {
+        DB_NAME: 'FicheECTS',
+        RDS_HOST: instance.instanceEndpoint.hostname,
+        DB_USER:'admin',
+        DB_PASSWORD:"4az0,=sVt1JH40sQZ1B4CpW4,_sYv3",
+        TABLE_NAME:"Formation",
+      },
+      vpc,
+      memorySize:128,
+      timeout: cdk.Duration.seconds(3),
+    });
 
-
-
+    //grant lambda function to access the database
+    instance.connections.allowDefaultPortFrom(FormationGet);
+    instance.grantConnect(FormationGet);
+    //add lambda function's security group to the database security group
+    instance.connections.allowFrom(FormationGet.connections,ec2.Port.tcp(3306));
+    //Create a new API Gateway  
+    const api = new apigateway.RestApi(this, 'FichesECTS', {
+      restApiName: 'FichesECTS Service',
+      description: 'This service serves FichesECTS.',
+    });
+    //Create a new resource
+    const formation = api.root.addResource('formation');
+    //Create a new method
+    const formationGetIntegration = new apigateway.LambdaIntegration(FormationGet);
+    formation.addMethod('GET', formationGetIntegration);
+    
 
 
 
   }
 }
-
+  
 
