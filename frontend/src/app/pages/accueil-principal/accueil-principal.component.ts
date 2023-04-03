9bb42e5c5746e573075f5e0c49d6232bbb1bf427
salimdrch <salim.drch@gmail.com>
2023-04-03 12:26:06 +0200

    aucun changement depuis le dernier push

--- frontend/src/app/pages/accueil-principal/accueil-principal.component.ts
+++ frontend/src/app/pages/accueil-principal/accueil-principal.component.ts
@@ -3,8 +3,10 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalFormationComponent } from '../modal-formation/modal-formation.component';
 import { FormationGetService } from 'src/app/services/formation-get.service';
 import { GestionFichesService } from 'src/app/services/gestion-fiches.service';
-import { FormationI } from 'src/app/modeles/formation-i';
 import { ChangeDetectorRef } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { FormationI } from 'src/app/modeles/formation-i';
+
 
 
 
@@ -16,20 +18,22 @@ import { ChangeDetectorRef } from '@angular/core';
 
 export class AccueilPrincipalComponent implements OnInit {
 
-  f: FormationI = <FormationI>{};
 
-  constructor(public formationService: FormationGetService, private modalService: NgbModal, public gestionService: GestionFichesService, private changeDecor: ChangeDetectorRef) {}
+  data$: Observable<FormationI>;
+
+
+  constructor(public formationService: FormationGetService, private modalService: NgbModal, public gestionService: GestionFichesService, private changeDecor: ChangeDetectorRef) {
+    this.data$ = of(<FormationI>{});
+  }
 
   ngOnInit(): void {
     this.gestionService.someMethod()
-
   }
 
   /** TODOO alerte en cas de manque de donnée ! */
   openModal(id : number) {
-    this.f = <FormationI>{}
     this.gestionService.getFormationUeById(id).subscribe(
-      () => { this.changeDecor.markForCheck(); },
+      (new_data) => { this.data$ = of(new_data) },
       (error) => {console.log('error : ', error)},
     );
     const modalRef = this.modalService.open(ModalFormationComponent);
