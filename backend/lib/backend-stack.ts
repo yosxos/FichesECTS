4251b4f87b1c264dd6fe03b05546ad60a1f52743
yasait <yassirx2012@gmail.com>
2023-04-03 15:32:58 +0200

    fixing auth

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -109,6 +109,8 @@ dbSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3306), 'Allow in
       generateSecret: false,
       authFlows: {
         userPassword: true,
+        //userSrp: true
+        
       }
     });
     //Define the authorize for your apiGateway
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+import { Amplify } from 'aws-amplify';
+import awsmobile from './aws-exports';
+Amplify.configure(awsmobile);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
