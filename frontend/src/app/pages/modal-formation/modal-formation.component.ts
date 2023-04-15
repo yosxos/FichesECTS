ce22dc08ba6dc3392d26393012ce90d0a9df8e31
titouanriot <titouanriot@orange.fr>
2023-04-15 22:00:17 +0200

    pdf tableau avec couleur

--- frontend/src/app/pages/modal-formation/modal-formation.component.ts
+++ frontend/src/app/pages/modal-formation/modal-formation.component.ts
@@ -57,5 +57,26 @@ export class ModalFormationComponent implements OnInit, OnDestroy {
     });
     doc.save('ficheEcts_' + this.data?.parcour + '_' + this.data?.niveau + '_' + this.data?.annee + '.pdf')
   }
-}
 
+
+
+generatePDFDescription(): void {
+  console.log('entreé');
+  
+  const element: Element | null = document.getElementById("htmlData");
+  if (element){
+  let myWindows = window.open('',);
+  if(myWindows){
+  myWindows.document.write("<!DOCTYPE html><html><head><meta charset='utf-8'><link rel='stylesheet' type='text/css' media='screen''>"
+  +"<style>.matiere {background-color: blue;}.general {background-color: yellow;}.generalUE {background-color: rgb(138, 129, 63);}.indication {background-color: white;}#UE1 {background-color: rgb(144, 241, 0);}#UE2 {background-color: rgb(90, 219, 233);}table {margin-left: 30px ;}td {border-width: 1px;}section {width: 1000px;}</style>");
+  myWindows.document.write('</head><body>');
+  myWindows.document.write(element.innerHTML);
+  myWindows.document.write("hello world");
+  myWindows.document.write('</body></html>');
+  myWindows.document.close();
+  myWindows.print()
+  myWindows.close();
+  }  
+}
+}
+}
